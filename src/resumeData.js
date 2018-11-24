cf213e586d1df5aa6c00810675f2de6feda49fdc
sundariya <suraj0533@gmail.com>
2018-11-24 22:25:26 +0530

    minor  changes

--- src/resumeData.js
+++ src/resumeData.js
@@ -26,7 +26,7 @@ let resumeData = {
     "contactNumber": "8962340487",
     "emailAddress": "suraj0533@gmail.com",
     "address":"India",
-    "website":"https://resume-risbjrhbwy.now.sh",
+    "website":"",
     "education":[
       {
         "UniversityName":"National Institute of Technology Raipr",
